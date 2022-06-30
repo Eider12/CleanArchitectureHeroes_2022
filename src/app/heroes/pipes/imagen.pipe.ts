84f15a17365e98697abd9163992c17a782e6cc1c
EiderGit <eider.1211.ov@gmail.com>
2022-06-30 03:07:39 -0400

    Crud Heroes

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -2,13 +2,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  // pure: false
+  
+  // Esto se pone para detectar a cualquier cambio pequno en el ciclo de vida, en este caso es para actualizar la imagen,
+  // por default es Pure: true
 })
 export class ImagenPipe implements PipeTransform {
 
   transform( heroe: Heroe ): string  {
 
-    return `assets/heroes/${ heroe.id }.jpg`;
-
+    if( !heroe.id && !heroe.alt_img ) {
+      return 'assets/no-image.png';
+    } else if( heroe.alt_img ){
+      return heroe.alt_img;
+    } else {
+      return `assets/heroes/${ heroe.id }.jpg`;
+    }
   }
 }
